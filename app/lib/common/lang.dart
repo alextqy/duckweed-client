7bcda9424d450907813fbf1960cdb9b9473809f8
alextqy <289959263@qq.com>
2023-08-29 17:05:58 +0800

    coding

--- app/lib/common/lang.dart
+++ app/lib/common/lang.dart
@@ -5,16 +5,33 @@ import "package:app/common/file.dart";
 class Lang {
   String type = "";
   String title = "";
+
   String requestTimedOut = "";
   String cancel = "";
+  String signIn = "";
+  String signOut = "";
+  String network = "";
+  String forgotPassword = "";
+  String signUp = "";
 
   Lang() {
     this.type = FileHelper().jsonRead(key: "lang", filePath: "config.json");
     this.title = FileHelper().jsonRead(key: "title", filePath: "config.json");
+
     if (this.type == "cn") {
       this.requestTimedOut = "请求超时";
+      this.signIn = "登录";
+      this.signOut = "退出";
+      this.network = "网络";
+      this.forgotPassword = "找回密码";
+      this.signUp = "注册";
     } else {
-      this.requestTimedOut = "request timed out";
+      this.requestTimedOut = "Request timed out";
+      this.signIn = "Sign in";
+      this.signOut = "Sign out";
+      this.network = "Network";
+      this.forgotPassword = "Forgot Password";
+      this.signUp = "Sign Up";
     }
   }
 }
--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,4 +1,6 @@
 import "package:flutter/material.dart";
+import "package:flutter/cupertino.dart";
+import "package:app/common/lang.dart";
 
 void main() {
   runApp(const RootApp());
@@ -15,33 +17,95 @@ class RootApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const HomePage(title: "Duck Weed"),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key, required this.title});
-
   final String title;
+
+  const HomePage({super.key, required this.title});
 
   @override
   State<HomePage> createState() => HomePageState();
 }
 
 class HomePageState extends State<HomePage> {
+  int? groupValue = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: const Center(
+    Size screenSize = MediaQuery.of(context).size;
+
+    Drawer actionMenu(BuildContext context) {
+      return Drawer(
+        width: screenSize.width * 0.5,
+        backgroundColor: Colors.black54,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
+          children: [
+            const Expanded(child: SizedBox()),
+            ListTile(
+              horizontalTitleGap: 20,
+              leading: const Icon(Icons.search_outlined, size: 20),
+              title: Text(
+                Lang().forgotPassword,
+                style: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 15),
+              ),
+              onTap: () async {
+                Navigator.of(context).pop();
+              },
+            ),
+            const Expanded(child: SizedBox()),
+          ],
+        ),
+      );
+    }
+
+    return Scaffold(
+      endDrawer: actionMenu(context),
+      appBar: AppBar(
+        toolbarHeight: 40,
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(
+          widget.title,
+          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.white70),
+        ),
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        margin: const EdgeInsets.all(0),
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            const Expanded(child: SizedBox()),
+            CupertinoSlidingSegmentedControl<int>(
+              backgroundColor: Colors.white70,
+              thumbColor: Colors.deepPurpleAccent,
+              padding: const EdgeInsets.all(3),
+              groupValue: groupValue,
+              children: {
+                0: groupValue == 0 ? selectedTabFont(Lang().network) : unselectedTabFont(Lang().network),
+                1: groupValue == 1 ? selectedTabFont(Lang().signIn) : unselectedTabFont(Lang().signIn),
+                2: groupValue == 2 ? selectedTabFont(Lang().signUp) : unselectedTabFont(Lang().signUp),
+              },
+              onValueChanged: (value) {
+                setState(() {
+                  groupValue = value;
+                });
+              },
+            ),
+          ],
         ),
       ),
     );
+  }
+
+  selectedTabFont(String t) {
+    return Text(t, style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 15, color: Colors.white));
+  }
+
+  unselectedTabFont(String t) {
+    return Text(t, style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 15, color: Colors.black));
   }
 }
