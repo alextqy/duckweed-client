bbf7896217abe320bd02d0e0ef815470abdb3cec
alextqy <289959263@qq.com>
2023-09-20 17:55:44 +0800

    coding

--- app/lib/interface/home_page.dart
+++ app/lib/interface/home_page.dart
@@ -5,9 +5,11 @@ import "package:flutter/cupertino.dart";
 import "package:app/common/lang.dart";
 import "package:file_selector/file_selector.dart";
 
+import "package:app/notifier/base_notifier.dart";
 import "package:app/notifier/announcement_notifier.dart";
 import "package:app/notifier/dir_notifier.dart";
 
+import "package:app/interface/common/show_alert_dialog.dart";
 import "package:app/interface/common/menu.dart";
 import "package:app/interface/common/pub_lib.dart";
 import "package:app/interface/common/marquee.dart";
@@ -28,6 +30,7 @@ class HomePageState extends State<HomePage> with TickerProviderStateMixin {
   DirNotifier dirNotifier = DirNotifier();
 
   bool showMarquee = true;
+  int parentID = 0;
 
   void fetchAnnouncementData() {
     announcementNotifier.announcements(url: appUrl).then((value) {
@@ -40,14 +43,27 @@ class HomePageState extends State<HomePage> with TickerProviderStateMixin {
     });
   }
 
+  basicListener() async {
+    showSnackBar(context, content: Lang().loading, backgroundColor: bgColor(context), duration: 1);
+    if (dirNotifier.operationStatus.value == OperationStatus.success) {
+      // fetchData();
+      showSnackBar(context, content: Lang().complete, backgroundColor: bgColor(context));
+    } else {
+      showSnackBar(context, content: dirNotifier.operationMemo, backgroundColor: bgColor(context));
+    }
+  }
+
   @override
   void initState() {
     fetchAnnouncementData();
+    dirNotifier.addListener(basicListener);
     super.initState();
   }
 
   @override
   void dispose() {
+    dirNotifier.removeListener(basicListener);
+    dirNotifier.dispose();
     super.dispose();
   }
 
@@ -61,6 +77,53 @@ class HomePageState extends State<HomePage> with TickerProviderStateMixin {
             child: Text(Lang().newFolder, style: textStyle(fontSize: 18)),
             onPressed: () async {
               Navigator.pop(context);
+              showDialog(
+                context: context,
+                barrierDismissible: true,
+                builder: (BuildContext context) {
+                  TextEditingController textController = TextEditingController();
+                  return UnconstrainedBox(
+                    constrainedAxis: Axis.vertical, // 取消原有宽高限制
+                    child: StatefulBuilder(
+                      builder: (BuildContext context, Function state) {
+                        return Dialog(
+                          child: Container(
+                            margin: const EdgeInsets.all(0),
+                            padding: const EdgeInsets.all(20),
+                            height: screenSize(context).height * 0.2,
+                            width: 200,
+                            child: Column(
+                              children: [
+                                TextFormField(
+                                  style: textStyle(),
+                                  controller: textController,
+                                  textAlign: TextAlign.center,
+                                  decoration: InputDecoration(
+                                    suffixIcon: IconButton(
+                                      icon: Icon(Icons.clear, size: iconSize, color: iconColor),
+                                      onPressed: () async => textController.clear(),
+                                    ),
+                                  ),
+                                ),
+                                const Expanded(child: SizedBox()),
+                                TextButton(
+                                  child: Text("OK", style: textStyle()),
+                                  onPressed: () {
+                                    if (textController.text.isNotEmpty) {
+                                      dirNotifier.dirAction(url: appUrl, dirName: textController.text, parentID: parentID, id: 0);
+                                      Navigator.pop(context);
+                                    }
+                                  },
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  );
+                },
+              );
             },
           ),
           CupertinoActionSheetAction(
